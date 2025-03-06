4ad60929aa6384a599adc254a9cfa5b6a77d3ff7
Eyob <Eyobeb337@gmail.com>
2025-03-06 16:55:18 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,19 +1,25 @@
-const WebSocket = require("ws");
+const { Server } = require("ws");
 
-const server = new WebSocket.Server({ noServer: true });
-
-server.on("connection", (ws) => {
-    ws.on("message", (message) => {
-        console.log("Received: " + message);
-        server.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    });
-});
+let server;
 
 module.exports = (req, res) => {
+    if (!server) {
+        server = new Server({ noServer: true });
+
+        server.on("connection", (ws) => {
+            ws.on("message", (message) => {
+                console.log("Received:", message);
+                server.clients.forEach((client) => {
+                    if (client.readyState === 1) {
+                        client.send(message);
+                    }
+                });
+            });
+        });
+
+        console.log("WebSocket server started");
+    }
+
     if (req.method === "GET") {
         res.status(200).send("WebSocket server is running");
     }
